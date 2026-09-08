6bc48f85559d82d8e411d282f1b2d32e620bb67f
fcaanaak <fcanak05@gmail.com>
2026-09-08 11:45:16 -0700

    Initial commit (dont commit to here directly anymore)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,35 @@
 import express, { type Express } from 'express';
 
+import IPService from "./services/ipService";
+import NameService from "./services/nameService";
+
+// Things completed
+// 1. Server IP
+// 2. Full name
+//
+// TODO:
+//
+// 1. Get the date and time of request in server time
+
+
 export function createApp(): Express {
   const app = express();
+
+  const ipService = new IPService();
+  const nameService = new NameService();
 
   app.get('/health', (_req, res) => {
     res.json({ status: 'ok' });
   });
+
+  app.get('/address', (_req, res,next) => {
+    const serverIP = ipService.getIPAddress();
+    serverIP ? res.json({ serverIP: ipService.getIPAddress()}) : res.status(500).json({ error: 'Error when getting server IP' });
+  })
+
+  app.get('/name', (_req, res) => {
+    res.json({name: nameService.getFullName()})
+  })
 
   app.use((_req, res) => {
     res.status(404).json({ error: 'Not Found' });
