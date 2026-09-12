980aa57ecb8c554a4f6c35a1a760f3790f58e199
fcaanaak <fcanak05@gmail.com>
2026-09-11 23:48:35 -0700

    Added basic hardcoded pixel grid composable

--- frontend/app/src/main/java/com/example/cpen321application/MainActivity.kt
+++ frontend/app/src/main/java/com/example/cpen321application/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,11 +19,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.cpen321application.ui.theme.CPEN321ApplicationTheme
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import androidx.core.graphics.toColorInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,10 +35,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             CPEN321ApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        apiBaseUrl = BuildConfig.API_BASE_URL,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    Column {
+                        Greeting(
+                            apiBaseUrl = BuildConfig.API_BASE_URL,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
+                        ColorGrid()
+                    }
+
                 }
             }
         }
@@ -40,6 +52,32 @@ class MainActivity : ComponentActivity() {
 }
 
 fun urlConcatenate(baseURL: String, endpoint: String) = "${baseURL.trimEnd('/')}/${endpoint}"
+
+
+// TODO: Add a websocket client here that will connect to the server's websocket endpoint
+// TODO: and update the grid with color values based on the JSON data each time an update is received
+// TODO: from the websocket server
+@Composable
+fun ColorGrid() {
+    var grid by remember { mutableStateOf(Array(16){Array(16){"#ffffff"} }) }
+
+    grid[8][8] = "#ff00ff"
+    grid[7][7] = "#ff00ff"
+    grid[7][8] = "#ff00ff"
+    grid[8][7] = "#ff00ff"
+
+    Column {
+        for (colorRow in grid) {
+            Row{
+                for (item in colorRow){
+                    Box(Modifier.background(Color(item.toColorInt())).padding(10.dp))
+                }
+            }
+        }
+
+    }
+
+}
 
 @Composable
 fun Greeting(apiBaseUrl: String, modifier: Modifier = Modifier) {
